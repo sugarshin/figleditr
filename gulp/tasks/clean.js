f0a4982684186a96199dc9deba9cc6ab611d53e3
sugarshin <shinsugar@gmail.com>
2015-09-11 01:55:42 +0900

    revert del

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -4,8 +4,5 @@ import del from 'del';
 import { clean as conf } from '../conf';
 
 gulp.task('clean', cb => {
-  del(conf.path).then(paths => {
-    console.log('Deleted:\n', paths.join('\n'));
-    cb();
-  });
+  del(conf.path, cb);
 });
