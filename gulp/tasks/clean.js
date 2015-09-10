ba60e1ace5f0a166717433fd2d5b398a5a322e24
sugarshin <shinsugar@gmail.com>
2015-09-11 01:25:17 +0900

    fix clean task

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -4,5 +4,8 @@ import del from 'del';
 import { clean as conf } from '../conf';
 
 gulp.task('clean', cb => {
-  del(conf.path, cb);
+  del(conf.path).then(paths => {
+    console.log('Deleted:\n', paths.join('\n'));
+    cb();
+  });
 });
