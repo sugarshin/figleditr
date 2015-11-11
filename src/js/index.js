31e9611a7b94ce4a9996f76b858c29000fb223fb
sugarshin <shinsugar@gmail.com>
2015-11-11 22:38:56 +0900

    rm bluebird

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import 'whatwg-fetch';
-import Promise from 'bluebird';
 import html2canvas from 'html2canvas';
 import React from 'react';
 import { render } from 'react-dom';
@@ -10,7 +9,6 @@ import App from './containers/App';
 import configureStore from './store/configureStore';
 import querySelector from './utils/querySelector';
 
-global.Promise = global.Promise || Promise;
 global.html2canvas = global.html2canvas || html2canvas;
 
 const store = configureStore();
